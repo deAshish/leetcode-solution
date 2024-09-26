f8d685d9c5c4b91bf60ca003652f7c1de82b1640
Ashish Ghimire <ashishghimireg@gmail.com>
2024-09-26 09:49:45 -0500

    Time: 33 ms, Memory: 44.8 MB - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -4,12 +4,9 @@ class Solution {
         int result = 0;
         // traverse through string to add each character into hashmap with value = repeated numbers.
         for(int i =0; i <s.length(); i++){
-            if(hm.containsKey(s.charAt(i))){
-                hm.put(s.charAt(i), hm.get(s.charAt(i)) + 1);
-            }else{
-                hm.put(s.charAt(i), 1);
-            }
+                hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
         }
+        
         // traverse through string to find out 1st non-repeated character
         for(int i=0; i<s.length(); i++){
             if(hm.get(s.charAt(i)) == 1)
