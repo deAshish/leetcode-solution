29c8aeebcd51fc79917e6af53e6c084ca18aa949
Ashish Ghimire <ashishghimireg@gmail.com>
2024-11-27 21:06:29 -0600

    Time: 2 ms (99.36%) | Memory: 48.7 MB (7.09%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,25 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n= nums.length;
-        int majority = n/3;
-        HashMap<Integer, Integer> elementCount = new HashMap<>();
+        int count1 = 0, count2 = 0, element1=-1, element2=-1;
         List<Integer> result = new ArrayList<>();
+        for(int element: nums){
+            if(element == element1){
+                count1++;
+            }
+            else if(element == element2){
+                count2++;
+            }
+            else if(count1 == 0){
+                element1 = element;
+                count1++;
+            }
+            else if(count2 == 0){
+                element2 = element;
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+
+        }
+
+        count1 = 0;
+        count2 = 0;
 
         for(int element: nums){
-            elementCount.put(element, elementCount.getOrDefault(element, 0)+1);
+            if(element1 == element) count1++;
+            if(element2 == element) count2++;
         }
 
-        for(Map.Entry<Integer, Integer> entry: elementCount.entrySet()){
-            if(entry.getValue() > majority){
-                result.add(entry.getKey());
-            }
+        if(count1 > n/3){
+            result.add(element1);
         }
+         if(count2 > n/3 && element1 != element2){
+            result.add(element2);
+        }
+        // if(result.size() == 2 && result.get(0) > result.get(1))
+        
 
-        if(result.size() == 2 && result.get(0) < result.get(1)){
-            int temp = result.get(0);
-            result.set(0, result.get(1));
-            result.set(1, temp);
-        }
         return result;
     }
 }
