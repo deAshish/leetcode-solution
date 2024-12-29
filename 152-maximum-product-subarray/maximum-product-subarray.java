c0e3907237111b54a5b741033b075edfc890dba3
Ashish Ghimire <ashishghimireg@gmail.com>
2024-12-29 10:48:25 -0600

    Time: 1 ms (88.44%) | Memory: 45.7 MB (8.65%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,31 +1,30 @@
 class Solution {
-    //method to calculate max value at current index
-    static int max(int i, int j, int k){
-        return Math.max(i, Math.max(j,k));
-    }
-    //method to calculate min value at current index
-    static int min(int i, int j, int k){
-        return Math.min(i, Math.min(j,k));
-    }
 
     public int maxProduct(int[] nums) {
-        //initialization for max product value at current index
-        int currentMax = nums[0];
-        //initialization for min product value at current index
-        int currentMin = nums[0];
-        //overall maximum product value
-        int maxProd = nums[0];
+        int n = nums.length;
+        int maxProduct = Integer.MIN_VALUE;
+        // Store product from left to right
+        int leftToRight = 1;
+        //store product from righ to left
+        int rightToLeft = 1;
 
-        for(int i=1; i< nums.length; i++){
-            //calculate max at current index and assign to temp variable
-            int temp = max(nums[i], nums[i]* currentMax, nums[i]*currentMin);
-            //calculate min at current index
-            currentMin = min(nums[i], nums[i]* currentMax, nums[i]*currentMin);
-            //assign current max value from temp
-            currentMax = temp;
-            //update max product value if any
-            maxProd = Math.max(maxProd, currentMax);
+        for(int i=0; i < n; i++){
+            if(leftToRight == 0){
+                leftToRight = 1;
+            }
+            if(rightToLeft == 0){
+                rightToLeft = 1;
+            }
+            //left to right product calculation
+            leftToRight *=nums[i];
+            
+            //calculate product from right to left
+            int j =n-i-1;
+            rightToLeft *= nums[j];
+
+            maxProduct = Math.max(leftToRight, Math.max(maxProduct, rightToLeft));
         }
-        return maxProd;
+
+        return maxProduct;
     }
 }
