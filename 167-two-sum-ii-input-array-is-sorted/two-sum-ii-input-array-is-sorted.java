d17e2591ac3615cabb46a558b342e96dc2305766
Ashish Ghimire <ashishghimireg@gmail.com>
2025-03-16 23:08:30 -0500

    Time: 2 ms (92.67%) | Memory: 47.5 MB (26.07%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,20 +1,28 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        // Same approach as normal  Two Sum Problem...
-        Map<Integer, Integer> hm = new HashMap<>();
+        
         int[] result = new int[2];
-        for(int i=0; i< numbers.length; i++){
-            if(!hm.containsKey(target - numbers[i])){
-                hm.put(numbers[i], i);
-            }
-            else{
-                result[0]=hm.get(target- numbers[i])+1;
-                result[1] = i+1;
+        int left =0, right = numbers.length-1;
+        while(left < right){
+            //Use left and right pointer
+            //Check if sum is greater, smaller or eqaul
+            //If greater increase left pointer, smaller decrease right pointer.
+            int current_Sum = numbers[left]+ numbers[right];
+            if(current_Sum == target){
+                result[0] = ++left;
+                result[1] = ++right;
                 return result;
             }
+
+            else if(current_Sum < target){
+                left++;
+
+            }else{
+                right--;
+
+            }
+
         }
         return result;
-
-      
     }
 }
